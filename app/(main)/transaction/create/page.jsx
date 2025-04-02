702e8f3b812926778d5b2eb0245ec64f120eabc7
shamthecodes <shamthecoder@gmail.com>
2025-04-02 16:18:00 +0530

    prepare for deployment

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -4,17 +4,22 @@ import React from "react";
 import AddTransactionForm from "../_components/transaction-form";
 import { getTransaction } from "@/actions/transaction";
 
+// This forces dynamic rendering (no static generation)
+export const dynamic = "force-dynamic";
+
 const AddTransactionPage = async ({ searchParams }) => {
   const accounts = await getUserAccounts();
 
   const editId = searchParams?.edit;
   let initialData = null;
   if (editId) {
-    const transaction = await getTransaction(editId);
-    initialData = transaction;
+    try {
+      const transaction = await getTransaction(editId);
+      initialData = transaction;
+    } catch (error) {
+      console.error("Error fetching transaction:", error);
+    }
   }
-
-  console.log(editId);
 
   return (
     <div className="max-w-3xl mx-auto px-5">
--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "randomuser.me",
-        },
-      ],
-    },
-  
-    experimental: {
-      serverActions: {
-        bodySizeLimit: "5mb",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
       },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
     },
-  };
-  
-  export default nextConfig;
+
+    serverComponentsExternalPackages: ["@arcjet/runtime"],
+  },
+};
+
+export default nextConfig;
